e86d68eabb1e9d05bc3eb5a1cfb4482142bc97c1
Mauricio Carrillo <92446004+maxelOA@users.noreply.github.com>
2023-02-19 20:32:42 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,9 @@
-python setup.py sdist bdist_wheel
-python3 setup.py sdist
-
-
-pip install wheel setuptools pip --upgrade
-pip3 install wheel setuptools pip --upgrade
-
 from setuptools import setup
 
 setuptools.setup(
 	name='py_erpy2set',
 	version='1.0',
+	url='https://github.com/maxelOA/py_erpy2set.git'
 	description='ergast-python-2-set',
 	author='Mauricio Carrillo',
 	author_email='mury_cpineda@hotmail.com',
