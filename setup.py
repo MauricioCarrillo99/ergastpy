43c6c17658e1ce79c25c2643b0af9ee79f674280
Mauricio Carrillo <92446004+maxelOA@users.noreply.github.com>
2023-02-19 20:42:55 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,15 @@
 from setuptools import setup
 
-setuptools.setup(
-	name='py_erpy2set',
-	version='1.0',
-	url='https://github.com/maxelOA/py_erpy2set.git'
-	description='ergast-python-2-set',
-	author='Mauricio Carrillo',
-	author_email='mury_cpineda@hotmail.com',
-	packages=['py_erpy2set'],
-	scripts=['bin/erpy2set']
-	zip_safe=False,
-	install_requires=['numpy',
-                        'pandas',
-                        'requests',
-                        'json',
-                        'pdp'
-	],
+setup(
+    # Needed to silence warnings (and to be a worthwhile package)
+    name='erpy2set',
+    url='https://github.com/maxelOA/py_erpy2set',
+    author='Mauricio Carrillo',
+    author_email='mury_cpineda@hotmail.com',
+    packages=['erpy2set'],
+    install_requires=['numpy','pandas','requests','json'],
+    version='1.0',
+    license='BUAP',
+    description='A extension of ergast developer API',
+    long_description=open('README.txt').read(),
 )
