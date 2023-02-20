45d3cbcda58b82882536dbbad735b3d0d16f8156
Mauricio Carrillo <92446004+maxelOA@users.noreply.github.com>
2023-02-19 21:12:06 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     # Needed to silence warnings (and to be a worthwhile package)
     name='erpy2set',
-    url='https://github.com/maxelOA/py_erpy2set',
+    url='https://github.com/maxelOA/py_erpy2set/',
     author='Mauricio Carrillo',
     author_email='mury_cpineda@hotmail.com',
     packages=['erpy2set'],
