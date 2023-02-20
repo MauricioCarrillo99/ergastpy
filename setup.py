7dc7796076da117f7418b02b26bbb9c149a97444
Mauricio Carrillo <92446004+maxelOA@users.noreply.github.com>
2023-02-20 11:35:20 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     author_email="mury_cpineda@hotmail.com",
     packages=['erpy2set'],
     package_dir={"erpy2set":"erpy2set"},
-    install_requires=["numpy >=1.21.6","pandas >= 1.3.5","requests >=2.25.1","json"],
+    install_requires=["numpy >=1.21.6","pandas >= 1.3.5","requests >=2.25.1"],
     version="1.0",
     license="unlicense",
     description="A extension of ergast developer API",
