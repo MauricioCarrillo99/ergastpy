f15930f4f86b5e02a2eb8a25a060fee6180f5c08
Mauricio Carrillo <92446004+maxelOA@users.noreply.github.com>
2023-02-19 20:26:07 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,13 +8,13 @@ pip3 install wheel setuptools pip --upgrade
 from setuptools import setup
 
 setuptools.setup(
-	name='py_erpy2set_tst',
+	name='py_erpy2set',
 	version='1.0',
 	description='ergast-python-2-set',
 	author='Mauricio Carrillo',
 	author_email='mury_cpineda@hotmail.com',
-	packages=['py_erpy2set_tst'],
-	scripts=['bin/erpy2set_tst']
+	packages=['py_erpy2set'],
+	scripts=['bin/erpy2set']
 	zip_safe=False,
 	install_requires=['numpy',
                         'pandas',
