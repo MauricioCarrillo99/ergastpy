7a9692bc7259a0446aa5f793baa5c63ab2674416
Mauricio Carrillo <92446004+maxelOA@users.noreply.github.com>
2023-02-20 12:11:01 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,11 @@
 from setuptools import setup
 
 setup(
-    name="erpy2set",
+    name="py_erpy2set",
     url="https://github.com/maxelOA/erpy2set.git",
     author="Mauricio Carrillo",
     author_email="mury_cpineda@hotmail.com",
     packages=['erpy2set'],
-    package_dir={"erpy2set":"erpy2set"},
     install_requires=["numpy >=1.21.6","pandas >= 1.3.5","requests >=2.25.1"],
     version="1.0",
     license="unlicense",
